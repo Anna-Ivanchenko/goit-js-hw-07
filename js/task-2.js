01db44405ba6df2bad81726de5e24e88b906ad1e
Anna Ivanchenko <bubachka1990@gmail.com>
2026-01-18 21:41:42 +0200

    add solution for task-2

--- js/task-2.js
+++ js/task-2.js
@@ -24,3 +24,16 @@ const images = [
     alt: "Lighthouse Coast Sea",
   }
 ];
+
+const galleryList = document.querySelector(".gallery");
+
+const galleryMarkup = images
+  .map(({ url, alt }) =>
+    `<li class="gallery-item">
+      <img src="${url}" alt="${alt}" class="gallery-image">
+      </li>`
+  )
+  .join("");
+
+galleryList.insertAdjacentHTML("beforeend", galleryMarkup);  
+galleryList.classList.add("gallery");
