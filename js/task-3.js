faef1a24b9f580bb782911527c7aa474efcfb2bf
Anna Ivanchenko <bubachka1990@gmail.com>
2026-01-20 01:38:32 +0200

    add solution for task-3

---
+++ js/task-3.js
@@ -0,0 +1,15 @@
+//   <input type="text" id="name-input" placeholder="Please enter your name" />
+// <h1>Hello, <span id="name-output">Anonymous</span>!</h1>
+    
+
+const nameInput = document.querySelector("#name-input");
+const nameOutput = document.querySelector("#name-output");
+
+nameInput.addEventListener('input', () => {
+  const trimmedValue = nameInput.value.trim();
+
+    nameOutput.textContent = trimmedValue === ''
+        ? 'Anonymous'
+        : trimmedValue;
+});
+
